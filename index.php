41cd16cd84c5a0d5db2b5f7138a51ee967fd4842
Si Jobling <simon.jobling@gmail.com>
2014-04-24 08:19:51 +0200

    Reorganised social components on holding page

--- index.php
+++ index.php
@@ -36,47 +36,49 @@
 			<h2>Customise the experience to your team preferences</h2>
 			<p>Create your own calendars by selecting just the matches you're interested in - your country, venue, group or fixture.</p>
 		</section>
-
+    
 		<form id="newsletter">
 			<h2>Get email updates about the site and fixtures</h2>
 			<input type="email" name="email-address" placeholder="Your email address" />
 			<button>Subscribe</button>
 		</form>
 
-		<div id="social">
-			<h2>Follow, like or +1</h2>
-			<p>We're on <a href="http://twitter.com/wcko">Twitter</a>, <a href="http://www.facebook.com/worldcupkickoff">Facebook</a> and <a href="https://plus.google.com/115685022501320155518" rel="publisher">Google+</a> for all the status updates</p> 
-			
-			<a href="https://twitter.com/worldcupko" class="twitter-follow-button" data-show-count="true" data-show-screen-name="true">Follow @worldcupko</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-      
-      <div id="fb-root"></div>
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=252313181472957";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-      
-      <div class="fb-like-box" data-href="http://www.facebook.com/worldcupkickoff" data-width="250" data-height="100" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false"></div>
-      
-      <!-- Place this tag where you want the widget to render. -->
-      <div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/115685022501320155518" data-rel="author"></div>
-      
-      <!-- Place this tag after the last widget tag. -->
-      <script type="text/javascript">
-      
-        (function() {
-          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-          po.src = 'https://apis.google.com/js/plusone.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-        })();
-      </script>
+		<section id="social">
 
+      <div class="twitter">
+        <a href="https://twitter.com/worldcupko" class="twitter-follow-button" data-show-count="true" data-show-screen-name="true">Follow @worldcupko</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      </div>
+      
+      <div class="facebook">
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=252313181472957";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        
+        <div class="fb-like-box" data-href="http://www.facebook.com/worldcupkickoff" data-width="250" data-height="100" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false">Like on Facebook</div>
+        
+      </div>
 			
+      <div class="google">      
+
+        <div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/115685022501320155518" data-rel="author">+1 on Google</div>
+        
+        <script type="text/javascript">
+          (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+        
+      </div>			
 			
-		</div>
+		</section>
 
 	</article>
 
