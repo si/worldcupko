688708e777a60cecbb79654b0e4d237657b7be28
Si Jobling <simon.jobling@gmail.com>
2014-05-12 22:23:12 +0100

    Rearranged holding and index pages
    Moved previous index page to holding and fixtures page to index

---
+++ holding.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+	<title>World Cup 2014  Kick Off - all the times for the World Cup in Brazil 2014</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Henny+Penny|Monoton' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="stylesheets/rio.css" />
+
+  <script src="//use.edgefonts.net/amatic-sc.js"></script> 
+  <script src="/js/jquery.1.10.2.min.js"></script> 
+  <script src="/js/jquery.lettering-0.6.1.min.js"></script> 
+
+</head>
+<body>
+
+	<header>
+		<h1>World Cup 2014 Kick Off</h1>
+		<p>Thursday 12th June 2014 <em>to</em> Sunday 13th July 2014 in Brazil</p>
+		
+		<script>  $(document).ready(function() { $('header h1').lettering(); }); </script>
+	</header>
+
+	<article>
+
+		<section id="download">
+			<h2>Download to your calendar</h2>
+			<p>Keep the kick off times handy on your mobile, tablet or desktop calendar including Microsoft Outlook, OSX, iOS, Android and Blackberry. It even automatically updates when teams qualify!</p> 
+		</section>
+
+		<section id="reminders">
+			<h2>Notification reminders wherever you are </h2>
+			<p>Get alerted to kick off times with push notifications or reminders through Twitter, Facebook and Google+.</p> 
+		</section>
+
+		<section id="customise">
+			<h2>Customise the experience to your team preferences</h2>
+			<p>Create your own calendars by selecting just the matches you're interested in - your country, venue, group or fixture.</p>
+		</section>
+    
+		<form id="newsletter">
+			<h2>Get email updates about the site and fixtures</h2>
+			<input type="email" name="email-address" placeholder="Your email address" />
+			<button>Subscribe</button>
+		</form>
+
+		<section id="social">
+
+      <div class="twitter">
+        <a href="https://twitter.com/worldcupko" class="twitter-follow-button" data-show-count="true" data-show-screen-name="true">Follow @worldcupko</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      </div>
+      
+      <div class="facebook">
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=252313181472957";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        
+        <div class="fb-like-box" data-href="http://www.facebook.com/worldcupkickoff" data-width="250" data-height="100" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false">Like on Facebook</div>
+        
+      </div>
+			
+      <div class="google">      
+
+        <div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/115685022501320155518" data-rel="author">+1 on Google</div>
+        
+        <script type="text/javascript">
+          (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+        
+      </div>			
+			
+		</section>
+
+	</article>
+
+	<footer>
+		<p>Another <a href="http://unstyled.com">Unstyled</a> project by <a href="http://sijobling.com/">Si</a></p>
+
+	</footer>
+
+</body>
+</html>
