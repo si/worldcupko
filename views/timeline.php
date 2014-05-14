65cbb4cb5b2b45008b7c04d1957576dafae1c660
Si Jobling <simon.jobling@gmail.com>
2014-05-14 08:14:37 +0100

    Team class name and clock icon on timeline events

--- views/timeline.php
+++ views/timeline.php
@@ -21,11 +21,13 @@
           endif;
         ?>
       <li class="event">
-        <i class="icon" data-icon="&#xe06e;"></i>
+        <i class="icon" data-icon="&#xe094;"></i>
         <span class="time" data-timestamp="<?php echo $event->start; ?>"><?php echo date('ga', strtotime($event->start)); ?></span>
         <h2>
           <?php if($event->home_team->name!='') : ?>
-            <span class="team"><?php echo $event->home_team->name; ?></span> v <span class="team"><?php echo $event->away_team->name; ?></span>
+            <span class="team <?php echo strtolower(str_replace(' ','-',$event->home_team->name)); ?>"><?php echo $event->home_team->name; ?></span> 
+            v 
+            <span class="team <?php echo strtolower(str_replace(' ','-',$event->away_team->name)); ?>"><?php echo $event->away_team->name; ?></span>
           <?php else: ?>
             <?php echo $event->summary; ?>
           <?php endif; ?>
