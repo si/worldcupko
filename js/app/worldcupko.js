f6eb81ecd2f0d647cd337ab3defc47dd3250e753
Si Jobling <simon.jobling@gmail.com>
2014-05-19 08:36:01 +0100

    Next game countdown
    Using jQuery.Countdown plugin and MomentJS to parse and present a live
    countdown

--- js/app/worldcupko.js
+++ js/app/worldcupko.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-  var element = $('#next').find('strong'),
-      kickoff = element.attr('title');
 
-  $(element).html(moment(kickoff).fromNow());
+  var element = $('#next').find('strong'),
+      timestamp = element.attr('title'),
+      kickoff;
+
+  kickoff = moment(timestamp).format('YYYY/MM/DD HH:mm');
+  
+  $(element).countdown(
+    kickoff, 
+    function(event) {
+      $(this).html(event.strftime(
+        '<span><em>%D</em> days</span> '
+        + '<span><em>%H</em> hours</span> '
+        + '<span><em>%M</em> minutes</span> '
+        + '<span><em>%S</em> seconds</span> '
+      ));
+    }
+  );
 
 });
