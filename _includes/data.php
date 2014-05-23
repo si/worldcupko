44042de2bd1c4c1c5c3813679b7c612db27c8c32
Si Jobling <simon.jobling@gmail.com>
2014-05-23 14:54:08 +0100

    Diagnose data curl request

--- _includes/data.php
+++ _includes/data.php
@@ -1,6 +1,5 @@
 <?php
 $endpoint = 'http://alpha.kickoffcalendars.com/calendars/view/5.json';
-$facebook_endpoint = 'http://graph.facebook.com/http://www.worldcupkickoff.com';
 
 // create a new cURL resource
 $ch = curl_init();
@@ -10,14 +9,9 @@ curl_setopt($ch, CURLOPT_URL, $endpoint);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
+var_dump($ch);
+var_dump($data);
 $json = ($data!='') ? json_decode($data) : '';
-
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, $facebook_endpoint);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$facebook_response = curl_exec($ch);
-$facebook_data = ($facebook_response!='') ? json_decode($facebook_response) : '';
 
 // close cURL resource, and free up system resources
 curl_close($ch);
