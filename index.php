451a0495ba0ff098afb770d2ad04f426e245d4d8
Si Jobling <simon.jobling@gmail.com>
2014-05-30 07:35:57 +0100

    FB script
    For like button tracking

--- index.php
+++ index.php
@@ -38,6 +38,14 @@ include('_includes/data.php');
   
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=1498133113743918&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
   <?php include('views/intro.php'); ?>
   <?php include('views/stories.php'); ?>
