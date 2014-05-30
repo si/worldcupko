b543a5b916dfe7f0771f6ff2c90902700b6b9945
Si Jobling <simon.jobling@gmail.com>
2014-05-30 08:30:29 +0100

    Analytics code

--- index.php
+++ index.php
@@ -58,6 +58,17 @@ include('_includes/data.php');
   
   </footer>
   
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-91583-7', 'worldcupkickoff.com');
+    ga('send', 'pageview');
+  </script>
+  
   <script src="/js/libs/jquery-2.0.3.min.js"></script>
   <script src="/js/libs/moment.min.js"></script>
 	<script src="/js/plugins/jquery.countdown.min.js"></script>
