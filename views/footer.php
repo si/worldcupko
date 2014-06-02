90168f572e11cff6c6b56c1d357e02e791a43c7e
Si Jobling <simon.jobling@gmail.com>
2014-06-02 21:25:56 +0100

    FIFA app advert with CSS updates and JS tracking for affiliate monitoring

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,6 @@
   <footer>
     <div class="partners">
+      <a href="https://itunes.apple.com/gb/app/fifa/id756904853?mt=8"><img src="/img/ads/fifa-ios-app-store.png" alt="FIFA Official iOS app with live World Cup information" /></a>
       <a href="http://world-cup.gambling-guru.com/"><img src="/img/ads/world-cup-betting-125px.png" alt="World Cup 2014 betting" /></a>
     </div>
     <div class="credits">
