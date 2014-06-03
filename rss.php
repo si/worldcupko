1a9dd172522384e28892a2c5843a6818873e7bf5
Si Jobling <simon.jobling@gmail.com>
2014-06-03 13:51:04 +0100

    Rebuilt RSS feed with SimpleXML

--- rss.php
+++ rss.php
@@ -2,36 +2,40 @@
 date_default_timezone_set('UTC');
 
 // Require
-require('_includes/CyrilPerrinRss.php');
 require('_includes/global.php');
 require('_includes/data.php');
 
 // HTTP header
-header('content-type:application/rss+xml');
+header('content-type:application/rss+xml; charset=UTF-8');
 
-// Example
-$rss = new CyrilPerrinRss(
-    'World Cup 2014 Kick Off', 'http://www.worldcupkickoff.com', 'All the World Cup 2014 kick off times, direct from Brazil to your device', 'en', '120', time()
-);
-$rss->setImage('World Cup 2014 Kick Off', 'img/world-cup-2014-kick-off.png', 'http://www.worldcupkickoff.com/');
+$xml = new SimpleXMLElement('<rss/>');
+$xml->addAttribute("version", "2.0");
+
+$channel = $xml->addChild("channel");
+ 
+$channel->addChild("title", 'World Cup 2014 Kick Off');
+$channel->addChild("link", 'http://www.worldcupkickoff.com');
+$channel->addChild("description", 'All the World Cup 2014 kick off times, direct from Brazil to your device');
+$channel->addChild("language", "en-GB");
+$channel->addChild('ttl','120');
+$channel->addChild('lastBuildDate', date('r', time()));
+
+// $rss->setImage('World Cup 2014 Kick Off', 'img/world-cup-2014-kick-off.png', 'http://www.worldcupkickoff.com/');
 
 
 foreach($json->events as $event) : 
 
   $date = date('l j F Y', strtotime($event->start));
 
-  $rss->addItem(
-      '1', // GUID
-      $event->summary, // Title
-      url('event',$event->summary), // URL
-      date('ga', strtotime($event->start)) . ', ' . $event->location . ', ' . ((strlen($event->group)==1) ? 'Group ' : '') . $event->group, // Description
-      ((strlen($event->group)==1) ? 'Group ' : '') . $event->group, // Category (Group)
-      'World Cup Kick Off', // Author
-      strtotime($event->start), // Publish time
-      '', // Comments
-      'http://www.worldcupkickoff.com/'  // Source
-  );
-  
+  $item = $channel->addChild("item");
+
+  $item->addChild("guid", 'WCKO2014' . $event->event_id);
+  $item->addChild("title", $event->summary);
+  $item->addChild("link", url('event',$event->summary));
+  $item->addChild("description",  date('ga', strtotime($event->start)) . ', ' . $event->location . ', ' . ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
+  $item->addChild("pubDate", date('r', strtotime($event->start)));
+  $item->addChild('category', ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
+    
 endforeach;
 
-echo $rss;
+echo $xml->asXML();
