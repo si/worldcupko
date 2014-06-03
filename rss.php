f4c24ab9d7db8e6d5e5e1dcb9629b79c23eb0db6
Si Jobling <simon.jobling@gmail.com>
2014-06-03 14:40:45 +0100

    Before variable setting for RSS feeds so different requirements can check at different frequencies (emails for 24 hours, tweets for 1 hour or 10 minutes)

--- rss.php
+++ rss.php
@@ -5,11 +5,17 @@ date_default_timezone_set('UTC');
 require('_includes/global.php');
 require('_includes/data.php');
 
+$base_url = 'http://' . $_SERVER['SERVER_NAME'];
+$feed_url = $base_url . '/rss.php';
+
+$before = (isset($_GET['before'])) ? $_GET['before'] : 'tomorrow';
+
 // HTTP header
 header('content-type:application/rss+xml; charset=UTF-8');
 
 $xml = new SimpleXMLElement('<rss/>');
 $xml->addAttribute("version", "2.0");
+$xml->addAttribute('xmlns:atom', "http://www.w3.org/2005/Atom");
 
 $channel = $xml->addChild("channel");
  
@@ -20,22 +26,28 @@ $channel->addChild("language", "en-GB");
 $channel->addChild('ttl','120');
 $channel->addChild('lastBuildDate', date('r', time()));
 
+$atom_link = $channel->addChild('atom:link');
+$atom_link->addAttribute('rel', 'self');
+$atom_link->addAttribute('type', 'application/rss+xml');
+$atom_link->addAttribute('href', $feed_url);
+
 // $rss->setImage('World Cup 2014 Kick Off', 'img/world-cup-2014-kick-off.png', 'http://www.worldcupkickoff.com/');
 
 
 foreach($json->events as $event) : 
 
-  $date = date('l j F Y', strtotime($event->start));
+  if(strtotime($event->start) < strtotime($before)) { 
 
-  $item = $channel->addChild("item");
+    $item = $channel->addChild("item");
+    $item->addChild("guid", $base_url . url('event',$event->summary));
+    $item->addChild("title", $event->summary);
+    $item->addChild("link", $base_url . url('event',$event->summary));
+    $item->addChild("description",  date('ga', strtotime($event->start)) . ', ' . $event->location . ', ' . ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
+    $item->addChild("pubDate", date('r', strtotime($event->start)));
+    $item->addChild('category', ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
 
-  $item->addChild("guid", 'WCKO2014' . $event->event_id);
-  $item->addChild("title", $event->summary);
-  $item->addChild("link", url('event',$event->summary));
-  $item->addChild("description",  date('ga', strtotime($event->start)) . ', ' . $event->location . ', ' . ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
-  $item->addChild("pubDate", date('r', strtotime($event->start)));
-  $item->addChild('category', ((strlen($event->group)==1) ? 'Group ' : '') . $event->group);
-    
+  }
+  
 endforeach;
 
 echo $xml->asXML();
