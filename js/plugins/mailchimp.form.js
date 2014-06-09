c5914c2cead975ab5f2e1d46500f7f0a8d0efbd2
Si Jobling <simon.jobling@gmail.com>
2014-06-09 07:28:30 +0100

    Isolate Mailchimp JS functionality

--- js/plugins/mailchimp.form.js
+++ js/plugins/mailchimp.form.js
@@ -1,46 +1,4 @@
-$(document).ready(function(){
-
-  /*
-  Initiate Next Game Countdown
-  */
-  var element = $('#next .countdown'),
-      timestamp = element.attr('title'),
-      kickoff;
-
-  kickoff = moment(timestamp).format('YYYY/MM/DD HH:mm');
-  
-  $(element).countdown(
-    kickoff, 
-    function(event) {
-      $(this).html(event.strftime(
-        '<span><strong>%D</strong> days</span> '
-        + '<span><strong>%H</strong> hours</span> '
-        + '<span><strong>%M</strong> minutes</span> '
-        + '<span><strong>%S</strong> seconds</span> '
-      ));
-    }
-  );
-  
-  /*
-  Filter events based on URL
-  */
-  var url = window.location.pathname,
-      selector;
-      
-  // Identify filter type (summary, date, location, group)
-  if(url!='/') {
-    selector = url.substring(1);
-    selector = selector.replace('/','-');
-    selector = '.' + selector;
-    console.log(selector);
-    
-    // Hide all, show filter
-    $('.event, .date').hide();
-    $(selector).slideDown('slow');
-    $(selector).closest('.day').children('.date').slideDown('slow');
-  }
-
-});
+/* Mailchimp JS */
 
 var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';fnames[4]='MMERGE4';ftypes[4]='text';
 try {
