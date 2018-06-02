d1e6a4b7ef327ee02fb7f90bea37191c1849968b
Si Jobling <simon.jobling@gmail.com>
2018-06-02 16:26:27 +0100

    Update credits to Si's website

--- views/footer.php
+++ views/footer.php
@@ -4,7 +4,7 @@
       <a href="http://world-cup.gambling-guru.com/"><img src="/img/ads/world-cup-betting-125px.png" alt="World Cup 2014 betting" /></a>
     </div>
     <div class="credits">
-      <p>Another <a href="https://kickofftimes.app/">KickOff Times</a> by <a href="http://twitter.com/Si">Si</a></p>
+      <p>Another <a href="https://kickofftimes.app/">KickOff Times</a> spin-off from <a href="https://sijobling.com/">Si</a></p>
       <p>World Cup is managed by FIFA. All data is republished in openly available data formats.</p>
       <p>Source control on <a href="https://bitbucket.org/">Bitbucket</a>. Releases by <a href="http://deployhq.com">DeployHQ</a>. Emails from <a href="http://eepurl.com/V88BX">Mailchimp</a>.</p>
     </div>
