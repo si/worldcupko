e451718b9068d73f790ffa5e0189b0edbbb58046
Simon Jobling <simonj@asos.com>
2019-03-17 18:51:23 +0000

    Update footer links - Change repo from BitBucket to Github - Change DeployHQ to Heroku - Remove World Cup gambling badge

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,10 @@
   <footer>
     <div class="partners">
       <a href="https://itunes.apple.com/gb/app/fifa/id756904853?mt=8"><img src="/img/ads/fifa-ios-app-store.png" alt="FIFA Official iOS app with live World Cup information" /></a>
-      <a href="http://world-cup.gambling-guru.com/"><img src="/img/ads/world-cup-betting-125px.png" alt="World Cup 2014 betting" /></a>
     </div>
     <div class="credits">
       <p>Another <a href="https://kickofftimes.app/">KickOff Times</a> spin-off from <a href="https://sijobling.com/">Si</a></p>
       <p>World Cup is managed by FIFA. All data is republished in openly available data formats.</p>
-      <p>Source control on <a href="https://bitbucket.org/">Bitbucket</a>. Releases by <a href="http://deployhq.com">DeployHQ</a>. Emails from <a href="http://eepurl.com/V88BX">Mailchimp</a>.</p>
+      <p>Source control on <a href="https://github.com/si/worldcupko">Github</a>. Hosted on <a href="https://www.heroku.com">Heroku</a>. Emails from <a href="http://eepurl.com/V88BX">Mailchimp</a>.</p>
     </div>
   </footer>
